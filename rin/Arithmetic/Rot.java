7a9741c518b1f4f885ced7cc9c68c094d4b08e99
jharinkesh <rinkeshjha7@gmail.com>
2020-02-15 14:51:04 +0530

    added arithmetic folder

---
+++ rin/Arithmetic/Rot.java
@@ -0,0 +1,15 @@
+class Rot {
+    public static void main(String... args){
+        String org = args[0];
+        for(int i=1;i<args.length;i++){
+            check(org,args[i]);
+        }
+    }
+
+    static void check(String org, String s){
+        if((s+s).contains(org)){
+            System.out.println("rotation");
+        }
+    }
+
+}

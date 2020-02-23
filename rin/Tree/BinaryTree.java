e46c2438393a076def2263b15cb8dbaae518947a
jharinkesh <rinkeshjha7@gmail.com>
2020-02-23 22:08:22 +0530

    added assignment 4

--- rin/Tree/BinaryTree.java
+++ rin/Tree/BinaryTree.java
@@ -6,15 +6,46 @@ public class BinaryTree {
         Node n2 = new Node(8);
         Node n3 = new Node(5);
         Node n4 = new Node(9);
+        Node n5 = new Node(10);
         root.left = n1;
         root.right = n2;
         n1.left = n3;
         n1.right = n4;
+        n4.right = n5;
         //preOrder(root);
         //inOrder(root);
         //postOrder(root);
-        System.out.println(findMax(root));
+        // System.out.println(findMax(root));
+        // System.out.println(search(root,81));
+        // System.out.println(height(root));
+        System.out.println(count(root));
+    }
 
+    static int count(Node root){
+        if(root == null)
+            return 0;
+        int n1 = count(root.left);
+        int n2 = count(root.right);
+        return n1 + n2 + 1;
+    }
+    
+    static int height(Node root){
+        if(root == null ||(root.left == null && root.right == null) )
+            return 0;
+        int lh = height(root.left);
+        int rh = height(root.right);
+        int max = lh>rh ? lh: rh;
+        return max + 1;
+    }
+
+    static boolean search(Node root, int k){
+        boolean found = false;
+        if(root!=null){
+            if(root.key == k)
+                return true;
+            return search(root.left,k) || search(root.right,k);
+        }
+        return found;
     }
 
     static int findMax(Node root){

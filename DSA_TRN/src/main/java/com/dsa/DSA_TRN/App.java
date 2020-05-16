f62c5d5c2604eb86a9a458c1987240f8c307f912
jharinkesh <rinkeshjha7@gmail.com>
2020-05-16 16:05:18 +0530

    restructuring done

---
+++ DSA_TRN/src/main/java/com/dsa/DSA_TRN/App.java
@@ -0,0 +1,13 @@
+package com.dsa.DSA_TRN;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

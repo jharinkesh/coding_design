f1def009af5d3568039e71ffff050c9f6093f788
jharinkesh <rinkeshjha7@gmail.com>
2020-07-25 23:55:59 +0530

    added dfs program

--- DSA_TRN/src/main/java/graphs/AdjacencyListDemo.java
+++ DSA_TRN/src/main/java/graphs/AdjacencyListDemo.java
@@ -2,11 +2,12 @@ package graphs;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 public class AdjacencyListDemo {
 
-  public static void main(String[] args) {
-    AdjacencyList ls = new AdjacencyList(5);
+    public static void main(String[] args) {
+	AdjacencyList ls = new AdjacencyList(7);
 //    ls.add(0, 1);
 //    ls.add(0, 2);
 //    ls.add(1, 2);
@@ -15,61 +16,109 @@ public class AdjacencyListDemo {
 //
 //    ls.remove(2, 3);
 //    ls.disp();
-    
-    ls.add(0,1);
-    ls.add(0,2);
-    ls.add(0,4);
-    ls.add(3, 4);
-    
-    ls.disp();
-    ls.bfs(0);
-    
-  }
+
+//	ls.add(0, 1);
+//	ls.add(0, 2);
+//	ls.add(0, 4);
+//	ls.add(3, 4);
+//
+//	ls.disp();
+//	ls.bfs(0);
+
+	ls.add(0, 1);
+	ls.add(0, 3);
+	ls.add(0, 5);
+
+	ls.add(1, 2);
+	ls.add(3, 4);
+	ls.add(5, 6);
+//	ls.dfs(0);
+//	ls.bfs(0);
+	ls.dfsR(0);
+    }
 }
 
 class AdjacencyList {
-  final int n;
-  final LinkedList<Integer>[] list;
+    final int n;
+    final LinkedList<Integer>[] list;
+    boolean[] visited;
 
-  AdjacencyList(int n) {
-    this.n = n;
-    list = new LinkedList[n];
-    for (int i = 0; i < list.length; i++) {
-      list[i] = new LinkedList<Integer>();
+    AdjacencyList(int n) {
+	this.n = n;
+	list = new LinkedList[n];
+	visited = new boolean[n];
+	for (int i = 0; i < list.length; i++) {
+	    list[i] = new LinkedList<Integer>();
+	}
     }
-  }
 
-  public void add(int u, int v) {
-    list[u].add(v);
-    list[v].add(u);
-  }
-
-  public void remove(Integer u, Integer v) {
-    list[u].remove(v);
-    list[v].remove(u);
-  }
-
-  public void disp() {
-    for (int i = 0; i < n; i++) {
-      System.out.println(i + " : " + list[i]);
+    public void add(int u, int v) {
+	list[u].add(v);
+	list[v].add(u);
     }
-  }
-  
-  public void bfs(int startNode) {
-	  boolean visited[] = new boolean[n];
-	  Queue<Integer> q = new LinkedList<Integer>();
-	  q.add(startNode);
-	  visited[startNode] = true;
-	  while(!q.isEmpty()) {
-		  int v = q.poll();
-		  System.out.println(v);
-		  for(int u:list[v]) {
-			  if(!visited[u]) {
-				  q.add(u);
-				  visited[u] = true;
-			  }
-		  }
-	  }
-  }
+
+    public void remove(Integer u, Integer v) {
+	list[u].remove(v);
+	list[v].remove(u);
+    }
+
+    public void disp() {
+	for (int i = 0; i < n; i++) {
+	    System.out.println(i + " : " + list[i]);
+	}
+    }
+
+    public void dfsR(int startNode) {
+	visited[startNode] = true;
+	System.out.println(startNode);
+	for (int i : list[startNode]) {
+	    if (!visited[i])
+		dfsR(i);
+	}
+    }
+
+    public void dfs(int startNode) {
+	Stack<Integer> s = new Stack<Integer>();
+	boolean[] visited = new boolean[n];
+	s.push(startNode);
+	visited[startNode] = true;
+	System.out.println(startNode);
+	while (!s.isEmpty()) {
+	    int v = s.peek();
+	    int w = getUnvisited(v, visited);
+	    if (w != -1) {
+		visited[w] = true;
+		s.push(w);
+		System.out.println(w);
+	    } else {
+		s.pop();
+	    }
+	}
+    }
+
+    int getUnvisited(int node, boolean[] visited) {
+	for (int i : list[node]) {
+	    if (!visited[i])
+		return i;
+	}
+	return -1;
+    }
+
+    public void bfs(int startNode) {
+	boolean visited[] = new boolean[n];
+	Queue<Integer> q = new LinkedList<Integer>();
+	q.add(startNode);
+	visited[startNode] = true;
+	while (!q.isEmpty()) {
+	    int v = q.poll();
+	    System.out.println(v);
+	    for (int u : list[v]) {
+		if (!visited[u]) {
+		    q.add(u);
+		    visited[u] = true;
+		}
+	    }
+	}
+    }
 
 }
